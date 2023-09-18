1c0223d82751159d2caef332d7358873282ccd82
LidzDev <lydia.cordell@gmail.com>
2023-09-18 13:51:21 +0100

    created update client form for the catsitter

--- controllers/clients_controller.py
+++ controllers/clients_controller.py
@@ -8,3 +8,8 @@ clients_blueprint = Blueprint("clients", __name__)
 def list_clients():
     clients = Client.query.all()
     return render_template("clients/index.jinja", title = "My Pet Client Management System", clients = clients)
+
+@clients_blueprint.route("/clients/<int:id>")
+def show_one_client(id):
+    client_to_show = Client.query.get(id)
+    return render_template("clients/show.jinja", title=f"Pet CMS Client: {client_to_show.first_name} {client_to_show.last_name}", client=client_to_show)
