5abbc6b8c2ff3ed55f1e8f5a797e4a666730a4b8
LidzDev <lydia.cordell@gmail.com>
2023-09-18 14:44:40 +0100

    post method works for update form and pets are showing

--- controllers/clients_controller.py
+++ controllers/clients_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from models.client import Client
 from app import db
 
@@ -13,3 +13,16 @@ def list_clients():
 def show_one_client(id):
     client_to_show = Client.query.get(id)
     return render_template("clients/show.jinja", title=f"Pet CMS Client: {client_to_show.first_name} {client_to_show.last_name}", client=client_to_show)
+
+@clients_blueprint.route("/clients/<int:id>", methods=["POST"])
+def update_client(id):
+    client = Client.query.get(id)
+    client.first_name = request.form["first_name"]
+    client.last_name = request.form["last_name"]
+    client.email = request.form["email"]
+    client.phone = request.form["phone"]
+    client.street = request.form["street"]
+    client.postcode = request.form["postcode"]
+    db.session.commit()
+    redirect_string = "/clients/" + str(id)
+    return redirect(redirect_string)
