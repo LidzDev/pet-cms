b27f63473f5de6208f2d98910adf392d07bf6a3c
LidzDev <lydia.cordell@gmail.com>
2023-09-19 16:33:01 +0100

    pets can be updated

--- controllers/pets_controller.py
+++ controllers/pets_controller.py
@@ -15,13 +15,22 @@ def show_client_pets(id):
     client_to_show = Client.query.get(id)
     return render_template("pets/client.jinja", title=f"Hi {client_to_show.first_name}", client=client_to_show)
 
-@pets_blueprint.route("/pets/add/<int:id>", methods=["POST"])
+@pets_blueprint.route("/pets/client/add/<int:id>", methods=["POST"])
 def save_client_pet(id):
     # print("anything happening here????")
     pet_name = request.form["pet_name"]
-    pet_age = request.form["age"]
+    pet_age = request.form["pet_age"]
     pet_to_save = Pet(pet_name=pet_name, age=pet_age, client_id=id)
     db.session.add(pet_to_save)
     db.session.commit()
     redirect_string = "/pets/client/" + str(id)
     return redirect(redirect_string)
+
+@pets_blueprint.route("/pets/update/<int:id>", methods=["POST"])
+def update_pet(id):
+    pet = Pet.query.get(id)
+    pet.pet_name = request.form["pet_name"]
+    pet.age = request.form["pet_age"]
+    db.session.commit()
+    referrer = request.referrer
+    return redirect(referrer)
